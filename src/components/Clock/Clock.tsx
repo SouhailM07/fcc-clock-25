8238baad03ec4a8629bfad57fd187c03bc44c9c5
souhail <lightshadow416@gmail.com>
2024-02-21 16:46:08 +0100

    project core was built

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,4 @@
+import "./clock.css";
+export default function Clock() {
+  return <>Clock</>;
+}
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as MyContainer } from "./MyContainer/MyContainer";
+export { default as Clock } from "./Clock/Clock";
+export { default as ControlPanel } from "./ControlPanel/ControlPanel";
